c9dfa12497ef789a0f19656cfd8dc3b58331b25f
Elizabeth <ek@elizabethkillgore.com>
2023-09-18 12:51:43 -0400

    like book lab

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded")
+
+
+fetch('http://localhost:3000/books')
+    .then(res => res.json()) 
+    .then(bookData => {
+        bookData.forEach(book => {
+            createBookList(book)
+        })
+      console.log(bookData)      
+})       
+
+function createBookList(book) {
+    
+    let bookTitlesList = document.createElement('li')
+    bookTitlesList.addEventListener('click', () => {
+    let bookImage = document.createElement('img')
+    bookImage.src = book.img_url
+    document.querySelector('#show-panel').append(bookImage)
+    let bookDesription = document.createElement('p')
+    bookDesription.innerText = book.description
+    document.querySelector('#show-panel').append(bookDesription)
+    
+
+    book.users.map(user => {
+        let bookUsersList = document.createElement('p')
+        bookUsersList.innerText = user.username 
+        document.querySelector('#show-panel').append(bookUsersList)
+    })
+
+
+    
+    
+   })
+    
+    
+
+    
+bookTitlesList.innerText = book.title   
+    
+document.querySelector('#list').append(bookTitlesList)
+
+    
+}
+
+
+
+
+});
